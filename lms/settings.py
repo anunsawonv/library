5e9f90688b40fc8f48b6530270af005abab055b6
anunsawonv <162651952+anunsawonv@users.noreply.github.com>
2024-03-08 14:38:20 +0800

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -23,16 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&4r(1^gc9k+7m48eq4%jd#n9f_*6wle=$q9d=a=vym^#mm-(pf'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-&4r(1^gc9k+7m48eq4%jd#n9f_*6wle=$q9d=a=vym^#mm-(pf'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
 
-ALLOWED_HOSTS = ['127.0.0.1']
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -104,7 +104,7 @@ DATABASES = {
     }
 }
 
-# DATABASES["default"] = dj_database_url.parse("postgres://amsaismsdbv1_8wlu_user:mnuVaKoW8VjVEsIu3ujcPj4Dbf5SwZV1@dpg-cne2fn2cn0vc73f90qh0-a.oregon-postgres.render.com/amsaismsdbv1_8wlu")
+DATABASES["default"] = dj_database_url.parse("postgres://amsaismsdbv1_8wlu_user:mnuVaKoW8VjVEsIu3ujcPj4Dbf5SwZV1@dpg-cne2fn2cn0vc73f90qh0-a.oregon-postgres.render.com/amsaismsdbv1_8wlu")
 
 
 # Password validation
